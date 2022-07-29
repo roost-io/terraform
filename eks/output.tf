f71b735626b816f8ec8ca0884eb42d96865373da
Mayank Gaikwad <8110509+mgdevstack@users.noreply.github.com>
2022-07-29 23:41:44 +0530

    Add KUBECONFIG in output

--- eks/output.tf
+++ eks/output.tf
@@ -1,5 +1,5 @@
-output "cluster_certificate_authority_data"{
-    value = module.eks.cluster_certificate_authority_data
+output "kubeconfig"{
+    value = module.eks.kubeconfig
 }
 
 output "eks_endpoint"{
