94dfed881c028126b3efc32a86eea3e47ffa2e08
Bhavika2101 <bhavika.baid@zb.io>
2022-08-02 12:48:23 +0530

    ZBIO-3504: Modify provider.tf

--- eks/provider.tf
+++ eks/provider.tf
@@ -1,26 +1,12 @@
-
-# terraform {
-#   backend "s3" {
-#     bucket = "eaas-demo-terraform-states"
-#     key = "roost-io-terraform-eks"
-#     region = "ap-south-1"
-#   }
-# }
-
-provider aws{
-  region = "ap-south-1"
+terraform {
+  backend "s3" {
+    bucket = "eaas-demo-terraform-states"
+    key = "roost-io-terraform-eks"
+    region = "ap-south-1"
+  }
 }
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  }
 }
